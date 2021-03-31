9d005a00f1a3b157bddf5fabebc678f4cfd36d2a
Jade <jadeve.jimenez@yahoo.com>
2021-03-30 22:44:40 -0700

    figuring out the first steps

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -2,3 +2,11 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var form = d3.select("#form");
+
+var button = d3.select("#filter.btn");
+
+var tbody = d3.select("tbody")
+
+form.on("click",runEnter);
+button.on("submit",runEnter);
