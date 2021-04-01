fc4b83a57572c8c7a8fece24c8e221fceae89757
Jade <jadeve.jimenez@yahoo.com>
2021-03-31 22:20:11 -0700

    getting picture in

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -6,7 +6,15 @@ var form = d3.select("#form");
 
 var button = d3.select("#filter.btn");
 
-var tbody = d3.select("tbody")
+var tbody = d3.select("tbody");
 
 form.on("click",runEnter);
 button.on("submit",runEnter);
+
+function runEnter(){
+    d3.event.preventDefault();
+    var input = d3.select("#datetime");
+    var inputVal = input.property("value");
+    console.log(inputVal);
+    console.log(tableData);
+};
