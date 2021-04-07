e4a96f5fd3719a4c3c8a017991afb953422d66cc
Jade <jadeve.jimenez@yahoo.com>
2021-04-06 17:00:34 -0700

    alter button?

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -8,7 +8,7 @@ var button = d3.select("#filter.btn");
 
 var tbody = d3.select("tbody");
 
-form.on("click",runEnter);
+form.on("click", runEnter);
 button.on("submit",runEnter);
 
 function runEnter(){
@@ -16,12 +16,27 @@ function runEnter(){
     var input = d3.select("#datetime");
     var inputVal = input.property("value");
     console.log(inputVal);
-    console.log(tableData);
+    //console.log(tableData);
     var filterUfo = tableData.filter(alien => alien.datetime === inputVal);
+
+    tbody.html("")
+
+    // console.log(filterUfo)
+
+    
+
+    filterUfo.forEach((ufo) => {
+        var row = tbody.append("tr");
+        Object.entries(ufo).forEach(function([key,value]){
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    })
 };
 
 tableData.forEach(function(ufo){
-    console.log(ufo);
+
+    //console.log(ufo);
     var row = tbody.append("tr");
     Object.entries(ufo).forEach(function([key,value]){
         var cell = row.append("td");
